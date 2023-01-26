8852380a2a74e20d49001f0465a1dea84f651bc3
Svitlana Synytsia <49615852+svetlankogr@users.noreply.github.com>
2023-01-26 17:24:17 +0100

    Start project (#4)
    * homepage
    
    * Update package.json
    
    * start project
    
    * add fonts and add folders

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from 'react-router-dom';
+export default function Dashboard() {
+  return (
+    <ul>
+      <li>
+        <NavLink to="home">HomePage</NavLink>
+      </li>
+      <li>
+        <NavLink to="statistics">StatisticsPage</NavLink>
+      </li>
+    </ul>
+  );
+}
