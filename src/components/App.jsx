8bbfa437e10acd6cd37137a2368d5b3e75703f4f
Svitlana Synytsia <49615852+svetlankogr@users.noreply.github.com>
2023-01-26 16:29:08 +0100

    Start project (#3)
    * homepage
    
    * Update package.json
    
    * start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './Navigation/Navigation';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
+import Dashboard from 'pages/Dashboard';
+import Statistics from 'pages/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route path="home" element={<p></p>} />
+        <Route path="statistics" element={<Statistics />} />
+      </Route>
+      <Route path="*" element={<p>Not Found</p>} />
+    </Routes>
   );
 };
