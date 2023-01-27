de451f2358cd260cf611063c0132c939bd323afc
egarra <garikhabuzov@gmail.com>
2023-01-27 16:00:43 +0200

    media version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,33 @@
 import { Link } from 'react-router-dom';
+import Media from 'react-media';
 import sprite from '../../icons/sprite.svg';
 import styles from './Header.module.css';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.linkWrapper}>
-        <Link>
+      <div className={styles.headerLinkWrapper}>
+        <Link to="home">
           <svg className={styles.headerIcon}>
             <use href={sprite + '#wallet'}></use>
           </svg>
         </Link>
-        <Link>
-          <h2 className={styles.title}>Wallet</h2>
+        <Link to="home">
+          <h2 className={styles.headerTitle}>Wallet</h2>
         </Link>
       </div>
-      <div>
-        <p>Name</p>
-        <Link>
-          <svg className={styles.iconLogOut}>
+      <div className={styles.headerLogOutWrapper}>
+        <p className={styles.headerLogOutName}>Name</p>
+        <button type='button' className={styles.headerLogOutBtn}>
+          <svg className={styles.headerIconLogOut}>
             <use href={sprite + '#exit'}></use>
           </svg>
-        </Link>
+          <Media query="(min-width: 767px)" render={() =>
+          (
+            <p className={styles.headerLogOutExit}>Exit</p>
+          )}
+        />
+        </button>
       </div>
     </header>
   );
