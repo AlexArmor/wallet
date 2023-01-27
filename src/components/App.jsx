54d4685a816f1a70b0a74640ce04e2203fad987b
Svitlana Synytsia <svetlankogr@gmail.com>
2023-01-27 17:04:55 +0100

    private and restricted route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,60 @@
 import { Route, Routes } from 'react-router-dom';
-import { Navigation } from './Navigation/Navigation';
+import { Layout } from './Layout/Layout';
 import Register from 'pages/Register/Register';
 import Login from 'pages/Login/Login';
 import Dashboard from 'pages/Dashboard/Dashboard';
 import Statistics from 'pages/Statistics/Statistics';
 import Home from 'pages/Home/Home';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getTransactionCategories } from 'redux/finance/transactionOperation';
 import { getCurrentUser } from 'redux/auth/authOperation';
 import { useEffect } from 'react';
+import { PrivateRoute } from 'HOCs/PrivateRoute';
+import { RestrictedRoute } from 'HOCs/RestrictedRoute';
+import { selectIsRefreshing } from 'redux/auth/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(getCurrentUser());
     dispatch(getTransactionCategories());
   }, [dispatch]);
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />}>
-        <Route path="home" element={<Home />} />
-        <Route path="statistics" element={<Statistics />} />
-      </Route>
-      <Route path="*" element={<p>Not Found</p>} />
-    </Routes>
+
+  return isRefreshing ? (
+    <p>...Loading</p>
+  ) : (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/dashboard" component={<Login />} />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/dashboard"
+                component={<Register />}
+              />
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute redirectTo="/login" component={<Dashboard />} />
+            }
+          >
+            <Route index path="home" element={<Home />} />
+            <Route path="statistics" element={<Statistics />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<p>Not Found</p>} />
+      </Routes>
+    </>
   );
 };
