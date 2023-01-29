9d7ce502b0d35cd17979902897490b7e0b27a6d3
Svitlana Synytsia <svetlankogr@gmail.com>
2023-01-29 21:54:55 +0100

    navigation and dashboard positions (desc & tab)

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,36 @@
 import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.css';
+import sprite from '../../icons/sprite.svg';
 
 export function Navigation() {
   return (
     <>
-      <ul>
-        <li>
-          <NavLink to="/login">LoginPage</NavLink>
+      <ul className={css.navList}>
+        <li className={css.navItem}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? css.navLinkActive : css.navLink
+            }
+          >
+            <svg className={css.icon}>
+              <use href={sprite + '#home'}></use>
+            </svg>
+            <p className={css.navText}>Home</p>
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/register">RegisterPage</NavLink>
-        </li>
-        <li>
-          <NavLink to="/dashboard">DashboardPage</NavLink>
+        <li className={css.navItem}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? css.navLinkActive : css.navLink
+            }
+            to="statistics"
+          >
+            <svg className={css.icon}>
+              <use href={sprite + '#schedule'}></use>
+            </svg>
+            <p className={css.navText}>Statistics</p>
+          </NavLink>
         </li>
       </ul>
     </>
