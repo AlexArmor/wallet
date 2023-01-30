033e75357f9a2ce19978eef01e3fb3de2432a6d2
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2023-01-30 22:55:50 +0200

    up

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,25 @@
+// import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { getAllTransaction } from 'redux/finance/transactionOperation';
 import css from './Balance.module.css';
 
 const Balance = () => {
+  const currentBalance = useSelector(state => state.auth.user.balance);
+  const updatedBalance = useSelector(state => state.finance.totalBalance);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch();
+  // }, [dispatch]);
+
   return (
     <div className={css.balanceContainer}>
       <p className={css.balanceText}>Your balance</p>
       <p className={css.balanceItem}>
-        ₴ <span className={css.balanceValue}>24 000.00</span>
+        ₴ {}
+        <span className={css.balanceValue}>
+          {currentBalance ? currentBalance : updatedBalance}
+        </span>
       </p>
     </div>
   );
