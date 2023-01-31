a7807768a8bd1098df231d19e44e2b61ec78b280
egarra <garikhabuzov@gmail.com>
2023-01-31 15:36:51 +0200

    Update MobileTransactionsList.jsx

--- src/components/MobileTransationsList/MobileTransactionsList.jsx
+++ src/components/MobileTransationsList/MobileTransactionsList.jsx
@@ -9,55 +9,114 @@ import { Loader } from 'components/Loader/Loader';
 
 export const MobileTransactionsList = () => {
   const categories = useSelector(selectAllCategories);
-  
+
   const transactions = useSelector(selectAllTransactions);
-  const loading = useSelector(selectLoading)
+  const loading = useSelector(selectLoading);
   return (
     <div className={css.containerMobile}>
-      {loading && <Loader/>}
+      {loading && <Loader />}
       <ul className={css.tableListMobile}>
         {transactions.map(item => {
           return (
-           <li className={css.cardListItem} key={item.id}>
-                <ul className={css.cardListItemList}>
-                  <li className={css.tableItemMobile}>
-                    <div className={item.type === 'INCOME' ? css.tableItemMobileBlockGreen : css.tableItemMobileBlock}>
-                        <span className={css.itemNameMobile}>Date</span>
-                        <span className={css.itemValueMobile}>{item.transactionDate.slice(2, item.transactionDate.length).split('-').reverse().join('.')}</span>
-                    </div>
-                  </li>
-                  <li className={css.tableItemMobile}>
-                    <div className={item.type === 'INCOME' ? css.tableItemMobileBlockGreen : css.tableItemMobileBlock} >
-                        <span className={css.itemNameMobile}>Type</span>
-                        <span className={css.itemValueMobile}>{item.type === 'INCOME' ? '+' : '-'}</span>
-                    </div>
-                  </li>
-                  <li className={css.tableItemMobile}>
-                    <div className={item.type === 'INCOME' ? css.tableItemMobileBlockGreen : css.tableItemMobileBlock} >
-                        <span className={css.itemNameMobile}>Category</span>
-                        <span className={css.itemValueMobile}>{categories.find(obj => obj.id === item.categoryId )?.name ?? "" }</span>
-                    </div>
-                  </li>
-                  <li className={css.tableItemMobile}>
-                    <div className={item.type === 'INCOME' ? css.tableItemMobileBlockGreen : css.tableItemMobileBlock} >
-                        <span className={css.itemNameMobile}>Comment</span>
-                        <span className={css.itemValueMobile}>{item.comment || 'No comment'}</span>
-                    </div>
-                  </li>
-                  <li className={css.tableItemMobile}>
-                    <div className={item.type === 'INCOME' ? css.tableItemMobileBlockGreen : css.tableItemMobileBlock} >
-                        <span className={css.itemNameMobile}>Sum</span>
-                        <span className={item.type === 'INCOME' ? css.itemSumIncome : css.itemSumExpense} >{item.amount.toString().split('-').join('')}</span>
-                    </div>
-                  </li>
-                  <li className={css.tableItemMobile}>
-                    <div className={item.type === 'INCOME' ? css.tableItemMobileBlockGreen : css.tableItemMobileBlock} >
-                        <span className={css.itemNameMobile}>Balance</span>
-                        <span className={css.itemValueMobile}>{item.balanceAfter}</span>
-                    </div>
-                  </li>
-                </ul>
-           </li>
+            <li className={css.cardListItem} key={item.id}>
+              <ul className={css.cardListItemList}>
+                <li className={css.tableItemMobile}>
+                  <div
+                    className={
+                      item.type === 'INCOME'
+                        ? css.tableItemMobileBlockGreen
+                        : css.tableItemMobileBlock
+                    }
+                  >
+                    <span className={css.itemNameMobile}>Date</span>
+                    <span className={css.itemValueMobile}>
+                      {item.transactionDate
+                        .slice(2, item.transactionDate.length)
+                        .split('-')
+                        .reverse()
+                        .join('.')}
+                    </span>
+                  </div>
+                </li>
+                <li className={css.tableItemMobile}>
+                  <div
+                    className={
+                      item.type === 'INCOME'
+                        ? css.tableItemMobileBlockGreen
+                        : css.tableItemMobileBlock
+                    }
+                  >
+                    <span className={css.itemNameMobile}>Type</span>
+                    <span className={css.itemValueMobile}>
+                      {item.type === 'INCOME' ? '+' : '-'}
+                    </span>
+                  </div>
+                </li>
+                <li className={css.tableItemMobile}>
+                  <div
+                    className={
+                      item.type === 'INCOME'
+                        ? css.tableItemMobileBlockGreen
+                        : css.tableItemMobileBlock
+                    }
+                  >
+                    <span className={css.itemNameMobile}>Category</span>
+                    <span className={css.itemValueMobile}>
+                      {categories.find(obj => obj.id === item.categoryId)
+                        ?.name ?? ''}
+                    </span>
+                  </div>
+                </li>
+                <li className={css.tableItemMobile}>
+                  <div
+                    className={
+                      item.type === 'INCOME'
+                        ? css.tableItemMobileBlockGreen
+                        : css.tableItemMobileBlock
+                    }
+                  >
+                    <span className={css.itemNameMobile}>Comment</span>
+                    <span className={css.itemValueMobile}>
+                      {item.comment || 'No comment'}
+                    </span>
+                  </div>
+                </li>
+                <li className={css.tableItemMobile}>
+                  <div
+                    className={
+                      item.type === 'INCOME'
+                        ? css.tableItemMobileBlockGreen
+                        : css.tableItemMobileBlock
+                    }
+                  >
+                    <span className={css.itemNameMobile}>Sum</span>
+                    <span
+                      className={
+                        item.type === 'INCOME'
+                          ? css.itemSumIncome
+                          : css.itemSumExpense
+                      }
+                    >
+                      {item.amount.toString().split('-').join('')}
+                    </span>
+                  </div>
+                </li>
+                <li className={css.tableItemMobile}>
+                  <div
+                    className={
+                      item.type === 'INCOME'
+                        ? css.tableItemMobileBlockGreen
+                        : css.tableItemMobileBlock
+                    }
+                  >
+                    <span className={css.itemNameMobile}>Balance</span>
+                    <span className={css.itemValueMobile}>
+                      {item.balanceAfter}
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </li>
           );
         })}
       </ul>
