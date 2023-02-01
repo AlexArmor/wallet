b6e705b45a86124974ed4405a14149baf18c162e
Volodymyr Babych <v.d.babych@gmail.com>
2023-02-01 09:47:54 +0200

    add stat styles

--- src/components/CustomSelect/CustomSelect.styled.js
+++ src/components/CustomSelect/CustomSelect.styled.js
@@ -1,135 +1,130 @@
-import {
-    selectUnstyledClasses,
-  } from '@mui/base/SelectUnstyled';
-  import OptionUnstyled, { optionUnstyledClasses } from '@mui/base/OptionUnstyled';
-  import PopperUnstyled from '@mui/base/PopperUnstyled';
+import { selectUnstyledClasses } from '@mui/base/SelectUnstyled';
+import OptionUnstyled, {
+  optionUnstyledClasses,
+} from '@mui/base/OptionUnstyled';
+import PopperUnstyled from '@mui/base/PopperUnstyled';
 import { styled } from '@mui/system';
 
-const blue = {
-    100: '#DAECFF',
-    200: '#99CCF3',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
-    900: '#003A75',
-  };
-  
-  const grey = {
-    50: '#f6f8fa',
-    100: '#eaeef2',
-    200: '#d0d7de',
-    300: '#afb8c1',
-    400: '#8c959f',
-    500: '#6e7781',
-    600: '#57606a',
-    700: '#424a53',
-    800: '#32383f',
-    900: '#24292f',
-  };
-  
-  
-  export const StyledButton = styled('button')(
-    ({ theme }) => `
-    font-family: IBM Plex Sans, sans-serif;
-    font-size: 0.875rem;
-    box-sizing: border-box;
-   width: 180px;
-   height: 50px;
-    padding: 12px;
-    text-align: left;
-    line-height: 1.5;
-    background: transparent;
-    border: 1px solid #000000;
-border-radius: 30px;
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  
-    transition-property: all;
-    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-    transition-duration: 120ms;
-  
-    &:hover {
-      border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
-    }
-  
-    &.${selectUnstyledClasses.focusVisible} {
-      border-color: ${blue[400]};
-      outline: 3px solid ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
-    }
-  
-    &.${selectUnstyledClasses.expanded} {
-      &::after {
-        content: '▴';
-      }
-    }
-  
-    &::after {
-      content: '▾';
-      float: right;
-    }
-    `,
-  );
-  export const StyledListbox = styled('ul')(
-    ({ theme }) => `
-    background: rgba(255, 255, 255, 0.7);
-    box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(25px);
-    border-radius: 20px;
-    font-family: IBM Plex Sans, sans-serif;
-    font-size: 0.875rem;
-    box-sizing: border-box;
-    padding: 6px 0;
-    margin: 0;
-    width: 182px;
-    height: 157px;
-    overflow: auto;
-    scrollbar-width: none;
-    outline: 0px;
-    border: none;
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+export const StyledButton = styled('button')`
+  font-family: 'Circe';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  text-align: left;
+  line-height: calc(24 / 16);
+  box-sizing: border-box;
+  width: 280px;
+  height: 50px;
+  padding: 12px 20px;
+  background: transparent;
+  border: 1px solid #000000;
+  border-radius: 30px;
 
-    &::-webkit-scrollbar {
-        width: 0;
-        height: 0;
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 120ms;
+
+  &:hover {
+    border-color: grey;
   }
-    `,
-  );
-  
-  export const StyledOption = styled(OptionUnstyled)(
-    ({ theme }) => `
-    list-style: none;
-    padding: 8px;
-    cursor: pointer;
-  
-    &:last-of-type {
-      border-bottom: none;
+
+  &.${selectUnstyledClasses.focusVisible} {
+    border-color: blue;
+    outline: 3px solid blue;
+  }
+
+  &.${selectUnstyledClasses.expanded} {
+    &::after {
+      content: '▴';
     }
-  
-    &.${optionUnstyledClasses.selected} {
-      background-color: white;
-      color: ${theme.palette.mode === 'dark' ? blue[100] : blue[900]};
-    }
-  
-    &.${optionUnstyledClasses.highlighted} {
-      background-color: white;
-      color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    }
-  
-    &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
-      background-color: white;
-      color: ${theme.palette.mode === 'dark' ? blue[100] : blue[900]};
-    }
-  
-    &.${optionUnstyledClasses.disabled} {
-      color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
-    }
-  
-    &:hover:not(.${optionUnstyledClasses.disabled}) {
-      background-color: white;
-      color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    }
-    `,
-  );
-  
-  export const StyledPopper = styled(PopperUnstyled)`
-    z-index: 1;
-  `;
+  }
+
+  &::after {
+    content: '▾';
+    float: right;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 160px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 182px;
+  }
+`;
+
+export const StyledListbox = styled('ul')`
+  background: rgba(255, 255, 255, 0.7);
+  box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(25px);
+  border-radius: 20px;
+  padding: 12px 0;
+  margin: 0;
+  width: 280px;
+  height: 157px;
+  overflow: auto;
+  scrollbar-width: none;
+  outline: 0px;
+  border: none;
+
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 160px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 182px;
+  }
+`;
+
+export const StyledOption = styled(OptionUnstyled)`
+  width: 280px;
+  font-family: 'Circe';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: calc(24 / 16);
+  list-style: none;
+  padding: 3px 20px;
+  cursor: pointer;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &.${optionUnstyledClasses.selected} {
+    background-color: white;
+  }
+
+  &.${optionUnstyledClasses.highlighted} {
+    background-color: white;
+  }
+
+  &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
+    background-color: white;
+  }
+
+  &.${optionUnstyledClasses.disabled} {
+    color: grey;
+  }
+
+  &:hover:not(.${optionUnstyledClasses.disabled}) {
+    background-color: white;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 160px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 182px;
+  }
+`;
+
+export const StyledPopper = styled(PopperUnstyled)`
+  z-index: 1;
+`;
